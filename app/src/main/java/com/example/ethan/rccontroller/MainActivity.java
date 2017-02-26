959561473d256456a19bb9db0032f4abb1dbf898
Gregg <peters_gregg@hotmail.com>
2017-02-26 15:39:42 -0500

    Updating Github

--- app/src/main/java/com/example/ethan/rccontroller/MainActivity.java
+++ app/src/main/java/com/example/ethan/rccontroller/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.ethan.rccontroller;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
 import android.widget.Button;
 import android.widget.TextView;
 import android.support.design.widget.FloatingActionButton;
@@ -10,11 +12,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.R.color;
 
 public class MainActivity extends AppCompatActivity {
 
+    int angle=141;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -27,11 +36,157 @@ public class MainActivity extends AppCompatActivity {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
+
         });
 
-    // Example of a call to a native method
-    TextView tv = (TextView) findViewById(R.id.sample_text);
-    tv.setText(stringFromJNI());
+       // Example of a call to a native method
+       TextView tv = (TextView) findViewById(R.id.sample_text);
+       tv.setText(stringFromJNI());
+
+       //Get reference to Right button as define in the layout
+       final Button RC_Right = (Button) findViewById(R.id.RC_Right);
+
+       RC_Right.setOnTouchListener(new View.OnTouchListener (){
+
+           private Handler mHandler;
+
+           TextView mtv = (TextView) findViewById(R.id.sample_text);
+           //int angle=currentAngle;
+           String cmd="turn=";
+
+           Runnable mAction = new Runnable() {
+               @Override public void run() {
+                   cmd = cmd+String.valueOf(angle);
+                   mtv.setText(cmd);
+                   SocketClient myClient = new SocketClient("192.168.1.6", 21567, cmd, mtv);
+                   myClient.execute();
+                   mHandler.postDelayed(this, 5);
+                   angle +=2;
+                   if (angle > 255) {angle=255;};
+                   cmd="turn=";
+               }
+           }; //End of mAction Method
+
+           @Override public boolean onTouch(View v, MotionEvent M)
+           {//Define what should be done when button is pressed
+
+               switch(M.getAction()) {
+                   case  MotionEvent.ACTION_DOWN:
+                       if (mHandler != null) return true;
+                       mHandler = new Handler();
+                       mHandler.postDelayed(mAction, 5);
+                       break;
+                   case MotionEvent.ACTION_UP:
+                       if (mHandler == null) return true;
+                       mHandler.removeCallbacks(mAction);
+                       mHandler = null;
+                       break;
+               }
+               return false;
+
+           }
+       }); //End of SetOnTouchListener for Right Button
+
+
+
+        //Get reference to Left button as define in the layout
+        final Button RC_Left = (Button) findViewById(R.id.RC_Left);
+
+        RC_Left.setOnTouchListener(new View.OnTouchListener (){
+
+            private Handler mHandler;
+
+            TextView mtv = (TextView) findViewById(R.id.sample_text);
+            String cmd="turn=";
+
+            Runnable mAction = new Runnable() {
+                @Override public void run() {
+                    cmd = cmd+String.valueOf(angle);
+                    mtv.setText(cmd);
+                    SocketClient myClient = new SocketClient("192.168.1.6", 21567, cmd, mtv);
+                    myClient.execute();
+                    mHandler.postDelayed(this, 5);
+                    angle -=2;
+                    if (angle < 0) {angle=0;};
+                    cmd="turn=";
+                }
+            }; //End of mAction Method
+
+            @Override public boolean onTouch(View v, MotionEvent M)
+            {//Define what should be done when button is pressed
+
+                switch(M.getAction()) {
+                    case  MotionEvent.ACTION_DOWN:
+                        if (mHandler != null) return true;
+                        mHandler = new Handler();
+                        mHandler.postDelayed(mAction, 5);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        if (mHandler == null) return true;
+                        mHandler.removeCallbacks(mAction);
+                        mHandler = null;
+                        break;
+                }
+                return false;
+
+            }
+        }); //End of SetOnTouchListener for Left Button
+
+        //Get reference to Forward button as define in the layout
+        final Button RC_Forward = (Button) findViewById(R.id.RC_Forward);
+        RC_Forward.setOnTouchListener(new View.OnTouchListener (){
+
+            TextView tv = (TextView) findViewById(R.id.sample_text);
+
+            @Override public boolean onTouch(View v, MotionEvent M)
+            {//Define what should be done when button is pressed
+
+                switch(M.getAction()) {
+                    case  MotionEvent.ACTION_DOWN:
+                        tv.setText("Going Forward");
+                        SocketClient myClient = new SocketClient("192.168.1.6", 21567, "forward", tv);
+                        myClient.execute();
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        tv.setText("Stopping");
+                        myClient = new SocketClient("192.168.1.6", 21567, "stop", tv);
+                        myClient.execute();
+                        break;
+                }
+                return false;
+
+            }
+        }); //End of SetOnTouchListener for Forward Button
+
+//Get reference to Reverse button as define in the layout
+        final Button RC_Backward = (Button) findViewById(R.id.RC_Backward);
+        RC_Backward.setOnTouchListener(new View.OnTouchListener (){
+
+            TextView tv = (TextView) findViewById(R.id.sample_text);
+
+            @Override public boolean onTouch(View v, MotionEvent M)
+            {//Define what should be done when button is pressed
+
+                switch(M.getAction()) {
+                    case  MotionEvent.ACTION_DOWN:
+                        tv.setText("Going Backward");
+                        SocketClient myClient = new SocketClient("192.168.1.6", 21567, "backward", tv);
+                        myClient.execute();
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        tv.setText("Stopping");
+                        myClient = new SocketClient("192.168.1.6", 21567, "stop", tv);
+                        myClient.execute();
+                        break;
+                }
+                return false;
+
+            }
+        }); //End of SetOnTouchListener for Reverse Button
+
+   }; //end of onCreate method
+
+
 /*
     final Button RC_Left = (Button) findViewById(R.id.RC_Left);
     RC_Left.setOnClickListener(new View.OnClickListener() {
@@ -42,14 +197,24 @@ public class MainActivity extends AppCompatActivity {
             }
     }); */
 
-    }
+
+
     //Call this method if turn right button clicked. RC_Right is defined in the XML for the view
     public void RC_Left(View view) {
         // Go Right code
         TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText("Going Left");
 
-        SocketClient myClient = new SocketClient("192.168.1.8", 8080, tv);
+      SocketClient myClient = new SocketClient("192.168.1.6", 21567, "turn=200", tv);
+      myClient.execute();
+    }
+
+    public void RC_Speed(View view) {
+        // Go Right code
+        TextView tv = (TextView) findViewById(R.id.sample_text);
+        tv.setText("Set Speed to 10");
+
+        SocketClient myClient = new SocketClient("192.168.1.6", 21567, "speed50", tv);
         myClient.execute();
     }
 
@@ -57,24 +222,37 @@ public class MainActivity extends AppCompatActivity {
         // Go Right code
         TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText("Going Right");
+
+        SocketClient myClient = new SocketClient("192.168.1.6", 21567, "turn=15", tv);
+        myClient.execute();
     }
 
     public void RC_Forward(View view) {
         // Go Right code
         TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText("Going Forward");
+
+        SocketClient myClient = new SocketClient("192.168.1.6", 21567, "forward", tv);
+        myClient.execute();
     }
 
     public void RC_Reverse(View view) {
         // Go Right code
         TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText("Backing Up");
+
+        SocketClient myClient = new SocketClient("192.168.1.6", 21567, "backward", tv);
+        myClient.execute();
     }
 
     public void RC_Stop (View view) {
         // Go Right code
         TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText("Stopping!");
+
+        SocketClient myClient = new SocketClient("192.168.1.6", 21567, "stop", tv);
+        myClient.execute();
+
     }
 
     @Override
--- app/src/main/java/com/example/ethan/rccontroller/SocketClient.java
+++ app/src/main/java/com/example/ethan/rccontroller/SocketClient.java
@@ -12,45 +12,68 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.*;
+import java.net.*;
 
-public class SocketClient extends AsyncTask<Void, Void, Void> {
+import android.app.Activity;
+import android.widget.EditText;
 
-    //Set Server parameters
+
+
+public class SocketClient extends AsyncTask<Void, Void, String> {
+
     String dstAddress;
     int dstPort;
+    String RC_Command = "";
     String response = "";
     TextView textResponse;
 
-    //Constructor for SocketClient Class??
-    SocketClient(String addr, int port, TextView textResponse) {
+    SocketClient(String addr, int port, String cmd, TextView textResponse) {
         dstAddress = addr;
         dstPort = port;
+        RC_Command = cmd;
         this.textResponse = textResponse;
     }
 
     @Override
-    protected Void doInBackground(Void... arg0) {
+    protected String doInBackground(Void... arg0) {
 
-        //Declare Null socket
         Socket socket = null;
 
         try {
-            socket = new Socket(dstAddress, dstPort); //Initiate Socket
+            //Open socket connection to server
+            socket = new Socket(dstAddress, dstPort);
+            DataOutputStream dout=new DataOutputStream(socket.getOutputStream());
+            dout.writeUTF(RC_Command);
+            dout.flush();
+            //response = "command sent to socket:"+RC_Command;
 
+            //Define buffer to transmit data to server
+          /*  PrintWriter out = new PrintWriter(new BufferedWriter(
+            new OutputStreamWriter(socket.getOutputStream())),
+             true);
+            out.println("left"); */
+/*
+            //Declare output stream for capturing return data from Server
             ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(
                     1024);
             byte[] buffer = new byte[1024];
 
             int bytesRead;
-            InputStream inputStream = socket.getInputStream(); //Read from Socket?
+
+            //Get the input stream from the socket
+            InputStream inputStream = socket.getInputStream(); */
 
 			/*
              * notice: inputStream.read() will block if no data return
 			 */
-            while ((bytesRead = inputStream.read(buffer)) != -1) {
+      /*      while ((bytesRead = inputStream.read(buffer)) != -1) {
                 byteArrayOutputStream.write(buffer, 0, bytesRead);
                 response += byteArrayOutputStream.toString("UTF-8");
-            }
+            } */
 
         } catch (UnknownHostException e) {
             // TODO Auto-generated catch block
@@ -74,7 +97,7 @@ public class SocketClient extends AsyncTask<Void, Void, Void> {
     }
 
     @Override
-    protected void onPostExecute(Void result) {
+    protected void onPostExecute(String result) {
         textResponse.setText(response);
         super.onPostExecute(result);
     }
